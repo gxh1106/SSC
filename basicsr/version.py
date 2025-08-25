a9343a0d10a05865356ba8d9d650610dda14e945
Xinghao Guo <1822884210@qq.com>
2025-08-25 22:00:31 +0800

    init

---
+++ basicsr/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Mon Jul 14 10:54:45 2025
+__version__ = '1.4.2'
+__gitsha__ = '2b213bc'
+version_info = (1, 4, 2)
---
+++ drct/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Mon Jun 23 21:51:47 2025
+__version__ = '0.1.0'
+__gitsha__ = 'unknown'
+version_info = (0, 1, 0)
